ec01c4dd183bad86e6c4f9d2ed06c527f9ec9636
Savva Kodeikin <savva.kodeikin@gmail.com>
2019-03-16 02:06:28 +0300

    Added Kotlin module

---
+++ kotlin/src/main/kotlin/KotlinBasics.kt
@@ -0,0 +1,11 @@
+import beans.User
+
+fun main(args: Array<String>) {
+    println("Let's learn some basics of (っ◕‿◕)っ Kotlin ")
+
+    // Creating a bean in one line, see User.kt
+
+    val users = listOf(User("Jacob", 31), User("Jane", 29))
+
+    users.forEach { println("Printing user's info: ${it}") }
+}
